0859e17cbb8afe48cfdc5d7a1c0f4b373091f895
anbead20 <adrianta1030@gmail.com>
2025-11-17 09:30:37 +0100

    Reorganizar el archivo bootstrap.php para definir constantes de rutas y mejorar la carga de configuración y autoloader.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * @package App\Bootstrap
+ */
 
-require_once APP_DIR . 'config/parametros.php';
-require_once VENDOR_DIR . '../vendor/autoload.php';
+// Definir constantes de rutas principales en el sistema de archivos. No en host (http)
+define('APP_ROOT', realpath(__DIR__ . '/../../'));
+define('APP_DIR', APP_ROOT . '/app');
+define('PUBLIC_DIR', APP_ROOT . '/public');
+define('VENDOR_DIR', APP_ROOT . '/vendor');
+define('VIEWS_DIR', APP_ROOT . '/views');
+
+// Cargar configuración básica de la aplicación
+require_once APP_DIR . '/config/parametros.php';
+
+// Cargar autoloader de Composer
+require_once VENDOR_DIR . '/autoload.php';
+
+use Dotenv\Dotenv;
+
+try {
+    $dotenv = Dotenv::createImmutable(APP_ROOT);
+    $dotenv->load();
+    $dotenv->required(['DBHOST', 'DBNAME', 'DBUSER', 'DBPASS', 'DBPORT'])->notEmpty();
+} catch (Exception $e) {
+    // Manejar la excepción si el archivo .env no se encuentra o no se puede cargar
+    error_log("Error loading .env file: " . $e->getMessage());
+}
